6ecc63f1a7890e00a9ff41793218d4733af432f8
white_devil <91869614+white-hacker010@users.noreply.github.com>
2022-09-13 16:49:46 +0530

    Update and rename requirements.txt to install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo [*] Simple bash Script to install some python dependencies
+pip3 install requests
+pip3 install colorama
+pip3 install requests
+echo [*] All Dependencies Successfully installed ....incase no error...!
